4c73c4dace0b0e1d4c11275a1201f88755dd72d5
Henri126 <118396736+Henri126@users.noreply.github.com>
2024-03-13 00:25:15 +0200

    0.4

--- script test.js
+++ script test.js
@@ -1,12 +1,43 @@
 document.addEventListener('DOMContentLoaded', function() {
     var videoInfoButton = document.getElementById("videoInfoButton");
+    var bandInfoButton = document.getElementById("bandInfoButton");
+    var bandInfoCard = document.getElementById("bandInfoCard");
     var videoInfoCard = document.getElementById("videoInfoCard");
+    var songInfoCard = document.getElementById("songInfoCard");
+    var songInfoButton = document.getElementById("songInfoButton");
+    var deathGripsCard = document.getElementById("deathGripsCard");
+    var deathGrips = document.getElementById("deathGripsButton");
 
     videoInfoButton.addEventListener("click", function() {
         if (videoInfoCard.style.display === "none" || videoInfoCard.style.display === "") {
             videoInfoCard.style.display = "block";
         } else {
             videoInfoCard.style.display = "none";
+        }
+    });
+
+    bandInfoButton.addEventListener("click", function() {
+        if (bandInfoCard.style.display === "none" || bandInfoCard.style.display === "") {
+            bandInfoCard.style.display = "block";
+        } else {
+           bandInfoCard.style.display = "none";
+        }
+    });
+
+    songInfoButton.addEventListener("click", function() {
+        if (songInfoCard.style.display === "none" || songInfoCard.style.display === "") {
+            songInfoCard.style.display = "block";
+        } else {
+           songInfoCard.style.display = "none";
+        }
+    });
+
+
+    deathGripsButton.addEventListener("click", function() {
+        if (deathGripsCard.style.display === "none" || deathGripsCard.style.display === "") {
+            deathGripsCard.style.display = "block";
+        } else {
+            deathGripsCard.style.display = "none";
         }
     });
 
@@ -35,4 +66,7 @@ document.addEventListener('DOMContentLoaded', function() {
     updateClock();
 
     videoInfoCard.style.display = "none";
+    bandInfoCard.style.display = "none";
+    songInfoCard.style.display = "none";
+    deathGripsCard.style.display = "none";
 });
